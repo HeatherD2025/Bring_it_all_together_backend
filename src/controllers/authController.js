45f750172e5b7722ffec0849043f305df689c011
Ryan <ryfly2016@gmail.com>
2025-05-05 08:42:36 -0600

    fix comparing for old password to changed password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -256,42 +256,41 @@ const updateUserProfile = async (req, res, next) => {
         message: "User not found",
       });
     }
-    if (password) {
-      const isPasswordValid = await bcrypt.compare(password, user.password);
+    // if (password) {
+    //   const isPasswordValid = await bcrypt.compare(password, user.password);
 
-      if (!isPasswordValid) {
-        return res.status(401).json({
-          statusCode: 4401,
-          message: "Login denied",
-        });
-      }
+    //   if (!isPasswordValid) {
+    //     return res.status(401).json({
+    //       statusCode: 4401,
+    //       message: "Login denied",
+    //     });
+    //   }
 
-      // Generate new hashed password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const updatedUser = await prisma.user.update({
-        where: { id: userId },
-        data: {
-          email: email,
-          password: hashedPassword,
-          firstName: firstName,
-          lastName: lastName,
-        },
-      });
+    // Generate new hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        email: email,
+        password: hashedPassword,
+        firstName: firstName,
+        lastName: lastName,
+      },
+    });
 
-      const token = jwt.sign(
-        // { id: user.id, username: user.email },
-        { id: updatedUser.id, username: updatedUser.email },
-        process.env.WEB_TOKEN
-      );
-      // Remove password from response
-      const { password: _, ...userWithoutPassword } = updatedUser;
+    const token = jwt.sign(
+      // { id: user.id, username: user.email },
+      { id: updatedUser.id, username: updatedUser.email },
+      process.env.WEB_TOKEN
+    );
+    // Remove password from response
+    const { password: _, ...userWithoutPassword } = updatedUser;
 
-      res.status(200).json({
-        user: userWithoutPassword,
-        token,
-      });
-    }
+    res.status(200).json({
+      user: userWithoutPassword,
+      token,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({
